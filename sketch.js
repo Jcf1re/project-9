295d956dc802403f244c85e50d682bbf26c1487e
Jcf1re <88686267+Jcf1re@users.noreply.github.com>
2021-08-10 15:24:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  if(keyIsDown(RIGHT_ARROW)){
+    background("red")
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    background("yellow")
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    background("blue")
+  }
+  if(keyIsDown(UP_ARROW)){
+    background("green")
+  }
+
+
+}
+
+
+
+
